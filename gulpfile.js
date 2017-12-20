08713a17677629d0247c6bbbc04478ad63914e8d
Tiny <455274906@qq.com>
2017-12-20 21:50:57 +0800

    Continue to write index.html

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+// var cleanCss = require('gulp-clean-css');
+var uglifyJs = require('gulp-uglify');
+var connect = require('gulp-connect');
+var rubySass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+
+
+// // 压缩CSS
+// gulp.task('minifyCss', function () {
+//     return gulp.src('./src/style/*.css')
+//     .pipe(cleanCss())
+//     .pipe(gulp.dest('./dist/style/'));
+// });
+
+// 编译Sass
+gulp.task('sass', function() {
+    return rubySass('./src/sass/*.scss', {
+        sourcemap: false,
+        style: 'compressed',
+    }).pipe(gulp.dest('./dist/css/'));
+});
+
+// 压缩JS
+gulp.task('minifyJs', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglifyJs())
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/js/'));
+});
+// 监听Html
+gulp.task('html', ['sass', 'minifyJs'], function() {
+    return gulp.src('./index.html').pipe(connect.reload());
+});
+
+// 监听
+gulp.task('default', ['sass', 'minifyJs'], function() {
+    // 开启服务器
+    connect.server({
+        port: 9001,
+        livereload: true
+    });
+    gulp.watch('./src/sass/*.scss', ['html']);
+    gulp.watch('./src/js/*.js', ['html']);
+});
